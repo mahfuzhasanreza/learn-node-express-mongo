78ff9407a1b4631eaae1e6317184ae32013ca0d6
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-11-25 20:15:34 +0600

    Create get all data api and myServer api

--- my-first-server/index.js
+++ my-first-server/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const myServer = require('./myServer.json');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Hello from Server');
 })
 
-app.get('/data', (req,res)=>{
+app.get('/data', (req, res) => {
     res.send('More data');
 })
 
-app.listen(port, ()=>{
+app.get('/server', (req, res) => {
+    res.send(myServer);
+})
+
+app.get('/server/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('I need data for id: ', id);
+    const server = myServer.find(server => server.id === id) || {};
+    res.send(server);
+})
+
+app.listen(port, () => {
     console.log(`My server is running on port: ${port}`);
 })
