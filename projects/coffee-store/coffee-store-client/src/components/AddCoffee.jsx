454cb70e24b3c49043bb064b29194fca5257bc61
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-12-02 11:39:03 +0600

    Create Add coffee from Client Side and get data

--- projects/coffee-store/coffee-store-client/src/components/AddCoffee.jsx
+++ projects/coffee-store/coffee-store-client/src/components/AddCoffee.jsx
@@ -1,54 +1,57 @@
 
 const AddCoffee = () => {
+    const handleAddCoffee = event => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const newCoffee = { name, quantity, supplier, taste, category, details, photo };
+        console.log(newCoffee);
+    }
+
     return (
-        <div>
-            <h2 className="mt-4 mx-4">Add Coffee</h2>
-            <form className="flex gap-3 my-4 mx-4">
-                {/* <label className="input input-bordered flex items-center gap-2">
-                    <input type="text" className="grow" placeholder="Search" />
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="h-4 w-4 opacity-70">
-                        <path
-                            fillRule="evenodd"
-                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                            clipRule="evenodd" />
-                    </svg>
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="h-4 w-4 opacity-70">
-                        <path
-                            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                        <path
-                            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                    </svg>
-                    <input type="text" className="grow" placeholder="Email" />
-                </label> */}
-                <label className="input input-bordered flex items-center gap-2 w-52">
-                    <input type="text" className="grow" placeholder="Coffee Name" />
-                </label>
-                <label className="input input-bordered flex items-center gap-2 w-52">
-                    <input type="text" className="grow" placeholder="Available Quantity" />
-                </label>
-                {/* <label className="input input-bordered flex items-center gap-2">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="h-4 w-4 opacity-70">
-                        <path
-                            fillRule="evenodd"
-                            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                            clipRule="evenodd" />
-                    </svg>
-                    <input type="password" className="grow" value="password" />
-                </label> */}
+        <div className="bg-[#F4F3F0] p-20">
+            <h2 className="mt-4 mx-4 text-3xl font-semibold">Add Coffee</h2>
+            <form onSubmit={handleAddCoffee} className="space-y-5 my-4 gap-11 mx-4">
+                {/* form name quantity row */}
+                <div className="md:flex w-full gap-5">
+                    <label className="input input-bordered flex items-center gap-2 md:w-1/2">
+                        <input type="text" name="name" className="grow" placeholder="Coffee Name" />
+                    </label>
+                    <label className="input input-bordered flex items-center gap-2 md:w-1/2">
+                        <input type="text" name="quantity" className="grow" placeholder="Available Quantity" />
+                    </label>
+                </div>{/* form supplier taste row */}
+                <div className="md:flex w-full gap-5">
+                    <label className="input input-bordered flex items-center gap-2 md:w-1/2">
+                        <input type="text" name="supplier" className="grow" placeholder="Supplier Name" />
+                    </label>
+                    <label className="input input-bordered flex items-center gap-2 md:w-1/2">
+                        <input type="text" name="taste" className="grow" placeholder="Taste" />
+                    </label>
+                </div>{/* form category details row */}
+                <div className="md:flex w-full gap-5">
+                    <label className="input input-bordered flex items-center gap-2 md:w-1/2">
+                        <input type="text" name="category" className="grow" placeholder="Category" />
+                    </label>
+                    <label className="input input-bordered flex items-center gap-2 md:w-1/2">
+                        <input type="text" name="details" className="grow" placeholder="Details" />
+                    </label>
+                </div>{/* form photo row */}
+                <div className="w-full gap-5">
+                    <label className="input input-bordered flex items-center gap-2">
+                        <input type="text" name="photo" className="grow" placeholder="Photo" />
+                    </label>
+                </div>
+                <input type="submit" value="Add Coffee" className="btn btn-block bg-gray-600 text-white" />
             </form>
         </div>
     );
