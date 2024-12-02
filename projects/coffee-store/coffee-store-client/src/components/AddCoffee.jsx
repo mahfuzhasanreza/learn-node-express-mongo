c46597670bac8a1f45cf3f9c89600a9f499923f7
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-12-02 11:46:19 +0600

    Send Coffee data to the server

--- projects/coffee-store/coffee-store-client/src/components/AddCoffee.jsx
+++ projects/coffee-store/coffee-store-client/src/components/AddCoffee.jsx
@@ -15,6 +15,19 @@ const AddCoffee = () => {
 
         const newCoffee = { name, quantity, supplier, taste, category, details, photo };
         console.log(newCoffee);
+
+        // send data to the server
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
     }
 
     return (
--- projects/coffee-store/coffee-store-server/index.js
+++ projects/coffee-store/coffee-store-server/index.js
@@ -26,7 +26,10 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
+    app.post('/coffee', async(req, res)=>{
+        const newCoffee = req.body;
+        console.log(newCoffee);
+    })
 
 
     // Send a ping to confirm a successful connection
