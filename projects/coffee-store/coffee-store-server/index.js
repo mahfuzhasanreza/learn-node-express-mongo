a0922c4c3d31e5815881c098edf5c70947756f09
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-12-02 20:02:43 +0600

    Update single coffee information

--- projects/coffee-store/coffee-store-server/index.js
+++ projects/coffee-store/coffee-store-server/index.js
@@ -41,6 +41,28 @@ async function run() {
             res.send(result);
         })
 
+        app.put('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedCoffee = req.body;
+
+            const coffee = {
+                $set: {
+                    name: updatedCoffee.name,
+                    quantity: updatedCoffee.quantity,
+                    supplier: updatedCoffee.supplier,
+                    taste: updatedCoffee.taste,
+                    category: updatedCoffee.category,
+                    details: updatedCoffee.details,
+                    photo: updatedCoffee.photo,
+                }
+            }
+
+            const result = await coffeeCollection.updateOne(filter, coffee, options);
+            res.send(result);
+        })
+
         app.post('/coffee', async (req, res) => {
             const newCoffee = req.body;
             console.log(newCoffee);
