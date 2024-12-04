f765e7cad58f7f74173d1dcd7968c4423de80870
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-12-05 01:51:33 +0600

    Added auth provider and user sign up using firebase

--- projects/coffee-store-with-auth/coffee-store-client-with-auth/src/main.jsx
+++ projects/coffee-store-with-auth/coffee-store-client-with-auth/src/main.jsx
@@ -12,6 +12,7 @@ import AddCoffee from './components/AddCoffee.jsx';
 import UpdateCoffee from './components/UpdateCoffee.jsx';
 import SignIn from './components/SignIn.jsx';
 import SignUp from './components/SignUp.jsx';
+import AuthProvider from './providers/AuthProvider.jsx';
 
 const router = createBrowserRouter([
   {
@@ -46,6 +47,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
