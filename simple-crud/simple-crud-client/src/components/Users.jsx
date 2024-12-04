bca415e15b135ceac1dcabacb7de30b271527057
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-12-04 22:38:03 +0600

    Delete A User From the Database using UI click button

--- simple-crud/simple-crud-client/src/components/Users.jsx
+++ simple-crud/simple-crud-client/src/components/Users.jsx
@@ -3,12 +3,31 @@ import { useLoaderData } from "react-router-dom";
 const Users = () => {
 
     const users = useLoaderData();
+
+    const handleDelete = _id => {
+        console.log("delete", _id);
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.deletedCount>0){
+                    alert('deleted successfully');
+                }
+            })
+    }
+
     return (
         <div>
             <h2>{users.length}</h2>
             <div>
                 {
-                    users.map(user => <p key={user._id}>{user.name}, {user.email}</p>)
+                    users.map(user => <p key={user._id}>{user.name}, {user.email}
+                        <button
+                            onClick={() => handleDelete(user._id)}
+                        >X</button>
+                    </p>)
                 }
             </div>
         </div>
--- simple-crud/simple-crud-server/index.js
+++ simple-crud/simple-crud-server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -43,6 +43,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/users/:id', async(req,res)=>{
+      const id=req.params.id;
+      console.log('delete the id:', id);
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
