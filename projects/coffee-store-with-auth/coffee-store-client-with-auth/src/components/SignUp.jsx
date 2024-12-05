a742e55d9c6a051db19e4ca7328b844bcecf5b91
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-12-05 12:48:43 +0600

    create users post api and save user to mongodb

--- projects/coffee-store-with-auth/coffee-store-client-with-auth/src/components/SignUp.jsx
+++ projects/coffee-store-with-auth/coffee-store-client-with-auth/src/components/SignUp.jsx
@@ -7,13 +7,33 @@ const SignUp = () => {
 
     const handleSignUp = e => {
         e.preventDefault();
-    
+
         const email = e.target.email.value;
+        const name = e.target.name.value;
         const password = e.target.password.value;
-    
+
         createUser(email, password)
             .then(result => {
-                console.log(result.user);
+                console.log('user created at firebase', result.user);
+                const createdAt = result?.user?.metadata?.creationTime;
+
+                const newUser = { name, email, createdAt }
+
+                // save new user to the db
+                fetch('http://localhost:5000/users', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(newUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        // console.log('user created to db', data);
+                        if (data.insertedId) {
+                            console.log("user created in db");
+                        }
+                    })
             })
             .catch(error => {
                 console.log(error.message);
@@ -32,6 +52,12 @@ const SignUp = () => {
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="name" name='name' className="input input-bordered" required />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
--- projects/coffee-store-with-auth/coffee-store-server-with-auth/index.js
+++ projects/coffee-store-with-auth/coffee-store-server-with-auth/index.js
@@ -28,6 +28,8 @@ async function run() {
 
         const coffeeCollection = client.db('coffeeDB').collection('coffee');
 
+        const userCollection = client.db('coffeeDB').collection('users');
+
         app.get('/coffee', async (req, res) => {
             const cursor = coffeeCollection.find();
             const result = await cursor.toArray();
@@ -74,6 +76,14 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await coffeeCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // users api
+        app.post('/users',async(req,res)=>{
+            const newUser = req.body;
+            console.log('new user: ', newUser);
+            const result = await userCollection.insertOne(newUser);
             res.send(result);
         })
 
