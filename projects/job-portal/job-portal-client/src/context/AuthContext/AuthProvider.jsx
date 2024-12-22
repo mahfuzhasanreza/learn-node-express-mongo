d3c1770a23109fd9a489070b76d931f62fd3e98b
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-12-23 02:38:28 +0600

    Error deubug and sign in user

--- projects/job-portal/job-portal-client/src/context/AuthContext/AuthProvider.jsx
+++ projects/job-portal/job-portal-client/src/context/AuthContext/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import AuthContext from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import auth from '../../firebase/firebase.init';
 
 const AuthProvider = ({ children }) => {
@@ -10,6 +10,11 @@ const AuthProvider = ({ children }) => {
     const createUser = (email, password) => {
         setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
     }
 
     useEffect(() => {
@@ -27,7 +32,8 @@ const AuthProvider = ({ children }) => {
     const authInfo = {
         user,
         loading,
-        createUser
+        createUser,
+        signInUser,
     }
 
     return (
